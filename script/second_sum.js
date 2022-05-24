5cbc638c70f43fca8d91c2eda1f6faf4aaa9921f
ACondori95 <97042212+ACondori95@users.noreply.github.com>
2022-05-24 12:49:25 -0300

    second requirement js

---
+++ script/second_sum.js
@@ -0,0 +1,16 @@
+function secondSum() {
+  const input = document.getElementById('second_input').value;
+  const output = document.getElementById('second_output');
+  const input_int = parseInt(input);
+  if (input_int !== NaN) {
+    output.innerHTML = 'Total: ' + sum(input_int);
+  }
+}
+
+function sum(number) {
+  let total = 0;
+  for (let i = 1; i <= number; i++) {
+    total += 1;
+  }
+  return total;
+}
