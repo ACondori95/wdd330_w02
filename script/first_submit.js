2436ff3832323ec6e4787a7a7e7878142b045ef0
ACondori95 <97042212+ACondori95@users.noreply.github.com>
2022-05-24 12:38:37 -0300

    first requirement js

---
+++ script/first_submit.js
@@ -0,0 +1,5 @@
+function firstSubmit() {
+  const input = document.getElementById('first_input').value;
+  const output = document.getElementById('first_output');
+  output.innerHTML = 'You entered: ' + input;
+}
